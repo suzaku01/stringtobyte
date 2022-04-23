7bb9a4654476c5c08c4192466e580399f47e158c
suzaku <89909040+suzaku01@users.noreply.github.com>
2022-04-23 09:57:00 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//Converts string hex to byte array
+//Input:6B656E73
+//Output:0x6B, 0x65, 0x6E, 0x73
+
+var fg = "";
+myfunc();
+
+void myfunc()
+{
+    Console.WriteLine("Input  : string byte array (e.g.  6B 65 6E 73 or 6B656E73);");
+    Console.WriteLine("Output : byte array;");
+    fg = Console.ReadLine();
+    fg = fg.Replace(" ", "");
+    var list = new List<byte>();
+    for (int i = 0; i < fg.Length / 2; i++)
+    {
+        list.Add(Convert.ToByte(fg.Substring(i * 2, 2), 16));
+    }
+    string res = "";
+    for (int t = 0; t < list.Count; t++)
+    {
+        if (t == list.Count - 1)
+        {
+            res = res + "0x" + list[t].ToString("X");
+        }
+        else
+        {
+            res = res + "0x" + list[t].ToString("X") + ", ";
+        }
+    }
+
+    Console.WriteLine(res);
+    myfunc();
+}
